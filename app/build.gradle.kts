6d737dc9e0041dab819b08ce4fdea48b32b61833
Tcibele98 <tiagosecundino98@gmail.com>
2026-09-14 12:48:20 -0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,9 @@ android {
         applicationId = "com.popwatch.mvp"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "0.1.0"
+        versionCode = 2
+        versionName = "0.1.1"
+    
     }
 
     buildTypes {
